465b8104f3cd4f5fc6401c1ffe68f26d5a380800
konkos <kon_kostas@windowslive.com>
2022-11-22 19:44:54 +0200

    renamed list

--- src/main/java/uom/opensource/advancedjava/streams/Main.java
+++ src/main/java/uom/opensource/advancedjava/streams/Main.java
@@ -6,35 +6,35 @@ import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> integers = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        List<Integer> integerList = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
-        Stream<Integer> stream = integers.stream();
+        Stream<Integer> stream = integerList.stream();
 
         System.out.println("----------- FOR EACH -------------------");
-        integers.stream().forEach(i -> System.out.println(i));
-        // integers.forEach(i => console.log(i));
+        integerList.stream().forEach(i -> System.out.println(i));
+        // integerList.forEach(i => console.log(i));
 
         System.out.println("----------- FILTER -------------------");
-        List<Integer> greaterThan5 = integers.stream().filter(i -> i > 5).toList();
+        List<Integer> greaterThan5 = integerList.stream().filter(i -> i > 5).toList();
         greaterThan5.forEach(System.out::println);
 
         System.out.println("----------- MAP -------------------");
-        integers.stream().map(i -> i * 2).forEach(i -> System.out.println(i));
+        integerList.stream().map(i -> i * 2).forEach(i -> System.out.println(i));
 
         System.out.println("----------- SUM -------------------");
-        int sum = integers.stream().mapToInt(i -> i).sum();
+        int sum = integerList.stream().mapToInt(i -> i).sum();
         System.out.println(sum);
 
         System.out.println("----------- AVERAGE -------------------");
-        double asDouble = integers.stream().mapToInt(i -> i).average().getAsDouble();
+        double asDouble = integerList.stream().mapToInt(i -> i).average().getAsDouble();
         System.out.println(asDouble);
 
         System.out.println("----------- FIND FIRST -------------------");
-        Optional<Integer> first = integers.stream().filter(i -> i > 5).findFirst();
+        Optional<Integer> first = integerList.stream().filter(i -> i > 5).findFirst();
         System.out.println(first.get());
 
         System.out.println("----------- FIND ANY -------------------");
-        Optional<Integer> anyFirst = integers.parallelStream().filter(i -> i > 5).findAny();
+        Optional<Integer> anyFirst = integerList.parallelStream().filter(i -> i > 5).findAny();
         System.out.println(anyFirst.get());
 
         /*
