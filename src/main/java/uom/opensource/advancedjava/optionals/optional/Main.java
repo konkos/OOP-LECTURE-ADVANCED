91f7667450237449d73dddb08a6549d9af62cfa1
konkos <kon_kostas@windowslive.com>
2022-11-22 19:30:28 +0200

    improve code readability in optionals

--- src/main/java/uom/opensource/advancedjava/optionals/optional/Main.java
+++ src/main/java/uom/opensource/advancedjava/optionals/optional/Main.java
@@ -6,8 +6,12 @@ public class Main {
     public static void main(String[] args) {
         int i = 5;
         Optional<User> byId = DB.findById(i);
-        if(byId.isPresent()) System.out.println(byId.get());
-        else System.out.println("NOT FOUND");
+
+        if (byId.isPresent()) {
+            User user = byId.get();
+            System.out.println(user);
+        } else
+            System.out.println("NOT FOUND");
 
     }
 }
